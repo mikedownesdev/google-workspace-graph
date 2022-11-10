edc7dd761606aa95e56fc51eaef0b8216c7ccfa4
Mike Downes <mikedownes.dev@gmail.com>
2022-11-10 12:29:43 -0500

    No msg

--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -4,14 +4,17 @@ export default function SearchContainer() {
     return (
         <div id="search-container">
             <div>
-                <input type="text"></input>
+                <input id="search-input" type="text"></input>
             </div>
             <div>
                 <ul>
                     <li>One</li>
                     <li>Two</li>
-                    <li></li>
+                    <li>Three</li>
                 </ul>
+            </div>
+            <div>
+                expand/collapse
             </div>
         </div>
     )
